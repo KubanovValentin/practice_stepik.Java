986edf7c8799d4c12a026c4a23674f79df43f9a3
valentin <delo.biss13@gmail.com>
2021-09-03 17:50:07 +0300

    practice_stepik.Java.решаем задачи и пишем код

--- src/practice_stepik/Main.java
+++ src/practice_stepik/Main.java
@@ -1,10 +1,34 @@
 package practice_stepik;
 
-import java.util.Scanner;
+import java.util.Scanner; //не забывать при исползовании сканера
 
 public class Main {
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println(sc.nextLine());
+//    }
+//программа  будет ждать, пока пользователь введет что-то, а затем выведет на печать полученную строку.
+//    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//
+//        int x = sc.nextInt();
+//        double y = sc.nextDouble();
+//        String s = sc.nextLine();
+//        sc.close();
+//    }
+//-------------------------------
+    //мы вводим число и должны получить вывод такого же числа
+//        public static void main(String[] args) {
+//            Scanner sc = new Scanner(System.in);
+//            String st = sc.nextLine();
+//            System.out.println(st);
+//            sc.close();
+//        }
+    //водим имя и должны получить приветствие с именем
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println(sc.nextLine());
+            Scanner sc = new Scanner(System.in);
+            String st = sc.nextLine();
+            System.out.println("Hello," + " " + st);
+            sc.close();
     }
 }
