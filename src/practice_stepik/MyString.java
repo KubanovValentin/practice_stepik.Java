ecad15431d0cca58de70d3b477263d0be991f37f
valentin <delo.biss13@gmail.com>
2021-09-05 18:55:25 +0300

    practice_stepik.Java.продолжаем решать задачи String

--- src/practice_stepik/MyString.java
+++ src/practice_stepik/MyString.java
@@ -1,7 +1,35 @@
 package practice_stepik;
-
+import java.util.Scanner;
 public class MyString {
     public static void main(String[] args) {
+//        String myVar = "Hello, World!";
+//        System.out.print(myVar);
 
+//        --------------------------------
+// На вход подаётся строка. Выведите её длину.
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        int x = str.length();
+//        System.out.println(x);
+//        sc.close();
+//----------------------------------------------
+//На вход подаётся строка текста. Выведите её первый и последний элемент в одну строку через пробел.
+//        Scanner sc = new Scanner(System.in);
+//        String str = sc.nextLine();
+//        char letter_first = str.charAt(0);
+//        char letter_last = str.charAt(str.length() - 1);
+//        System.out.println(letter_first + " " + letter_last);
+//        sc.close();
+
+//---------------------------------------------------------------------------
+//       На вход подаётся строка текста, затем слово.
+// Выведите на печать true, если слово содержится в строке, и false - если не содержится.
+        Scanner sc = new Scanner(System.in);
+        String str1 = sc.nextLine();
+        String str2 = sc.nextLine();
+        boolean value = str1.contains(str2);
+        System.out.println(value);
+        sc.close();
     }
+
 }
