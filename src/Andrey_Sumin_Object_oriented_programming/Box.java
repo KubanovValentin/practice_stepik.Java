a356ccc2ebc80c368ff0b47bef1f831433577251
valentin <delo.biss13@gmail.com>
2021-10-26 16:31:40 +0300

    ооп

---
+++ src/Andrey_Sumin_Object_oriented_programming/Box.java
@@ -0,0 +1,7 @@
+package Andrey_Sumin_Object_oriented_programming;
+
+public class Box {
+    double width;
+    double height;
+    double length;
+}
---
+++ src/Andrey_Sumin_Object_oriented_programming/Human.java
@@ -0,0 +1,7 @@
+package Andrey_Sumin_Object_oriented_programming;
+
+public class Human {
+    String name;
+    int age;
+    double weight;
+}
