6389cca692273e38e250c46f1002163d0537d19d
valentin <delo.biss13@gmail.com>
2021-11-02 15:42:10 +0300

    ооп.zaur.metod overloding

--- src/Zaur_OOP/Employee.java
+++ src/Zaur_OOP/Employee.java
@@ -1,24 +1,31 @@
 package Zaur_OOP;
 
 public class Employee {
+//это не корректный способ вызывать конструктор в конструкторе,в классе EmployeeConstrVnConstr будет правильный код
 
+    //ниже мы вызываем конструктор а в нутри его тоже конструктор
+    // с помощью this мы можем это сделать
+    // и выражение this должно быть первым как ниже
     Employee(int id2, String surname2, int age2) {
+        this(surname2, age2);
+
         id = id2;
-        surname = surname2;
-        age = age2;
+//      surname = surname2;
+//      age = age2;
     }
 
-    Employee(String surname2, int age2) {
-        surname = surname2;
-        age = age2;
+    Employee(String surname3, int age3) {
+        surname = surname3;
+        age = age3;
     }
 
-    Employee(int id2, String surname2, int age2, double salary2, String department2) {
-        id = id2;
-        surname = surname2;
-        age = age2;
-        salary = salary2;
-        department = department2;
+    Employee(int id4, String surname4, int age4, double salary4, String department4) {
+        this(id4, surname4, age4);
+//        id = id4;
+//        surname = surname4;
+//        age = age4;
+        salary = salary4;
+        department = department4;
     }
 
     int id;
---
+++ src/Zaur_OOP/EmployeeConstrVnConstr.java
@@ -0,0 +1,42 @@
+package Zaur_OOP;
+//более коректное написания конструктор в конструкторе
+
+
+public class EmployeeConstrVnConstr {
+    EmployeeConstrVnConstr(int id2, String surname2, int age2) {
+        this(id2, surname2, age2, 0.0, null);
+    }
+
+    EmployeeConstrVnConstr(String surname3, int age3) {
+        this(0, surname3, age3, 0.0, null);
+    }
+
+    EmployeeConstrVnConstr(int id4, String surname4, int age4, double salary4, String department4) {
+        id = id4;
+        surname = surname4;
+        age = age4;
+        salary = salary4;
+        department = department4;
+    }
+
+    int id;
+    String surname;
+    int age;
+    double salary;
+    String department;
+}
+
+class EmployeeConstrVnConstrTest {
+    public static void main(String[] args) {
+        EmployeeConstrVnConstr emp1 = new EmployeeConstrVnConstr(1, "Пупкин", 26);
+        System.out.println(emp1.surname);
+
+        EmployeeConstrVnConstr emp3 = new EmployeeConstrVnConstr("Белый", 56);
+        System.out.println(emp3.age);
+
+        EmployeeConstrVnConstr emp4 = new EmployeeConstrVnConstr(23, "Петухов", 34, 56.678, "IT");
+        System.out.println(emp4.surname);
+    }
+
+
+}
