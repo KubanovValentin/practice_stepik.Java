5a1eb9a4ded7172fe6bbd18536ba1cd9442026a8
valentin <delo.biss13@gmail.com>
2021-11-21 21:12:06 +0300

    практикум по программированию.задачи ConditionalOperator_2

---
+++ src/ProgrammingWorkshop/ConditionalOperator_2/IceСream/Main.java
@@ -0,0 +1,29 @@
+package ProgrammingWorkshop.ConditionalOperator_2.IceСream;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int a=3;
+        int b=5;
+        int k= sc.nextInt();
+
+        if (k%a==0||k%b==0||k%a%b==0||k%b%a==0){
+            System.out.println("YES");
+        }else {
+            System.out.println("NO");
+        }
+//        if (k%3==0||
+//                k%5==0||
+//                (k >= 6 && (k - 6) % 5 == 0)||
+//                (k >= 9 && (k - 9) % 5 == 0)||
+//                (k >= 12 && (k - 12) % 5 == 0)){
+//            System.out.println("YES");
+//        }else if(k==4&&k<3){
+//            System.out.println("NO");
+//        }else {
+//            System.out.println("NO");
+//        }
+    }
+}
---
+++ src/ProgrammingWorkshop/ConditionalOperator_2/Main.java
@@ -0,0 +1,4 @@
+package ProgrammingWorkshop.ConditionalOperator_2;
+
+public class Main {
+}
