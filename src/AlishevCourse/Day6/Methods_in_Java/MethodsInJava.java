e9f234e3905bb791127dc26d0cd37ce7eeb36abc
valentin <delo.biss13@gmail.com>
2021-11-28 19:02:34 +0300

    AlishevCourse/day5 && day6

--- src/AlishevCourse/Day6/Methods_in_Java/MethodsInJava.java
+++ src/AlishevCourse/Day6/Methods_in_Java/MethodsInJava.java
@@ -7,10 +7,12 @@ public class MethodsInJava {
         person1.name="Jon";
         person1.age=50;
         person1.speak();//у объекта person1 мы вызываем метод speak()
+        person1.haHaHa();
         Person person2=new Person();
         person2.name="Jeck";
         person2.age=30;
         person2.speak();//у объекта person2 мы вызываем метод speak()
+        person2.sayHello();
     }
 }
 class Person{
@@ -24,5 +26,14 @@ class Person{
     //создаем метод говорить
     void speak(){
         System.out.println("Меня зовут "+name+","+ "мне "+age+" лет");
+
+    }
+    void haHaHa(){
+        for (int i=0;i<3;i++){
+            System.out.println("Ха-Ха-Ха");
+        }
+    }
+    void sayHello(){
+        System.out.println("Привет!!!");
     }
 }
