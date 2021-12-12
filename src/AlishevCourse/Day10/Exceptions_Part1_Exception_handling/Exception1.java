723cbb8fd25c5be9588d67a2194e185915931602
valentin <delo.biss13@gmail.com>
2021-12-12 15:40:34 +0300

    AlishevCourse/day10.Exception

---
+++ src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Exception1.java
@@ -0,0 +1,2 @@
+package AlishevCourse.Day10.Exceptions_Part1_Exception_handling;public class Exception1 {
+}
---
+++ src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Test2/Exception2.java
@@ -0,0 +1,2 @@
+package AlishevCourse.Day10.Reading_from_a_file.Test.Test2;public class Exception2 {
+}
