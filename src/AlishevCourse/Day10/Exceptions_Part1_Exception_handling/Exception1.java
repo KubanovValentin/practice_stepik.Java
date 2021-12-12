454f387ee1016bf53e61a215280e83d2869c7dd4
valentin <delo.biss13@gmail.com>
2021-12-12 16:31:39 +0300

    AlishevCourse/day10

--- src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Exception1.java
+++ src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Exception1.java
@@ -1,2 +1,26 @@
-package AlishevCourse.Day10.Exceptions_Part1_Exception_handling;public class Exception1 {
+package AlishevCourse.Day10.Exceptions_Part1_Exception_handling;
+//Исключения (часть 1). Обработка исключений.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Exception1 {
+//    public static void main(String[] args) throws FileNotFoundException {
+//        File file=new File("qeqwewqe");
+//        Scanner sc=new Scanner(file);
+//    }
+        public static void main(String[] args) {
+            File file=new File("test35");
+            try {
+                Scanner sc=new Scanner(file);
+                System.out.println("после Scanner-выполнение блока try ");
+            } catch (FileNotFoundException e) {
+//                e.printStackTrace();
+                //в случае если появится исключение
+                System.out.println("Error.файл не найден");
+            }
+            //после блока try-catch код выполняется как обычно
+            System.out.println("после блока....");
+
+        }
 }
--- src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Test2/Exception2.java
+++ src/AlishevCourse/Day10/Exceptions_Part1_Exception_handling/Test2/Exception2.java
@@ -1,2 +1,18 @@
-package AlishevCourse.Day10.Reading_from_a_file.Test.Test2;public class Exception2 {
+package AlishevCourse.Day10.Exceptions_Part1_Exception_handling.Test2;
+//Исключения (часть 1). Обработка исключений.
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+public class Exception2 {
+    public static void main(String[] args) {
+        try {
+            readFile();
+        } catch (FileNotFoundException e) {
+            System.out.println("обработка исключения в методе main");
+        }
+    }
+    public static void readFile() throws FileNotFoundException {
+        File file=new File("test35");
+        Scanner sc=new Scanner(file);
+    }
 }
