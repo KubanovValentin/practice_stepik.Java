ba0a9f77ee223f6295e734f08150768c928cd60a
valentin <delo.biss13@gmail.com>
2021-12-16 14:15:15 +0300

    javarush.zadachi/13-15

---
+++ src/JavaRushTask/Zadachi_3_lv/Solution15.java
@@ -0,0 +1,21 @@
+package JavaRushTask.Zadachi_3_lv;
+/*
+Напиши программу, которая считывает с клавиатуры два вещественных числа (double) и выдает сообщение о том,
+равны ли эти числа с точностью до одной миллионной.
+ */
+import java.util.Scanner;
+
+public class Solution15 {
+    private static final double accuracy=0.000001;
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        double x= sc.nextDouble();
+        double y=sc.nextDouble();
+        if (Math.abs(y-x)<accuracy){
+            System.out.println("числа равны");
+        }else {
+            System.out.println("числа не равны");
+        }
+    }
+}
