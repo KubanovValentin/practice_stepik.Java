33002de4bc23b576f4beb2f681c80bbf08ff11d1
valentin <delo.biss13@gmail.com>
2021-12-17 21:46:13 +0300

    javarush.zadachi/4lv.3-4

---
+++ src/JavaRushTask/Zadachi_4/LV/Solution3.java
@@ -0,0 +1,20 @@
+package JavaRushTask.Zadachi_4.LV;
+
+/*
+Используя вложенные циклы while (цикл в цикле) выведи на экран прямоугольник размером 5 (высота) на 10 (ширина),
+заполненный буквой 'Q'.
+ */
+public class Solution3 {
+    public static void main(String[] args) {
+       int i=0;
+        while (i<5){
+            int n=0;
+            while (n<10){
+                System.out.print("Q");
+                n++;
+            }
+            System.out.println();
+            i++;
+        }
+    }
+}
