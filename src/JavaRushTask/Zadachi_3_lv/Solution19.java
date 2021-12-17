73879477429937383fa735dfb05920a81e996f90
valentin <delo.biss13@gmail.com>
2021-12-17 15:33:01 +0300

    javarush.zadachi/15-19.and lv app

---
+++ src/JavaRushTask/Zadachi_3_lv/Solution19.java
@@ -0,0 +1,17 @@
+package JavaRushTask.Zadachi_3_lv;
+
+import java.util.Scanner;
+
+/*
+Приведем только один простой пример: нужно ввести с клавиатуры две строки и определить, одинаковые они, или нет.
+Вот как будет выглядеть этот код:
+ */
+public class Solution19 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String a = console.nextLine();
+        String b = console.nextLine();
+        String result = a.equals(b) ? "Одинаковые" : "Разные";
+        System.out.println(result);
+    }
+}
