1e6b3927641c7e7fa900879f1aea43cee49d21e4
valentin <delo.biss13@gmail.com>
2021-12-18 13:41:13 +0300

    javarush.zadachi/4lv.5

---
+++ src/JavaRushTask/Zadachi_4/LV/Solution5.java
@@ -0,0 +1,22 @@
+package JavaRushTask.Zadachi_4.LV;
+
+import java.util.Scanner;
+
+/*
+Вводим данные с клавиатуры и сразу их выводим на экран, пока не будет введено слово "enough".
+Слово "enough" выводить не нужно.
+Для этого необходимо использовать бесконечный цикл (while(true)).
+ */
+public class Solution5 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        while (true)
+        {
+            String s = sc.nextLine();
+            if (s.equals("enough")){
+                break;}
+            System.out.println(s);
+        }
+
+    }
+}
