cd8cfc21fe5bdd13c3b58ffd1644fe768e4b5878
valentin <delo.biss13@gmail.com>
2022-02-12 12:52:42 +0300

    заур 15 урок

---
+++ src/Homework/homework_lesson15/Zaur_TimeChas.java
@@ -0,0 +1,32 @@
+package Homework.homework_lesson15;
+
+public class Zaur_TimeChas {
+    static void vremya() {
+        int chas = 0;
+        OUTER:
+        while (chas < 6) {
+            int minuta = -1;
+            MIDDLE:
+            do {
+                minuta++;
+                if (chas > 1 && minuta % 10 == 0) {
+                    break OUTER;
+                }
+                int sekunda = 0;
+                INNER:
+                while (sekunda < 60) {
+                    if (sekunda * chas > minuta) {
+                        continue MIDDLE;
+                    }
+                    System.out.println(chas + ":" + minuta + ":" + sekunda);
+                    sekunda++;
+                }
+            } while (minuta < 59);
+            chas++;
+        }
+    }
+
+    public static void main(String[] args) {
+        vremya();
+    }
+}
