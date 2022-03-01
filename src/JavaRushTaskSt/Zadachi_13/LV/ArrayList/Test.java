3467a6f44b4fdae56c09612b45f756c93c5e0bed
valentin <delo.biss13@gmail.com>
2022-03-01 16:53:06 +0300

    заур коллекции

---
+++ src/JavaRushTaskSt/Zadachi_13/LV/ArrayList/Test.java
@@ -0,0 +1,12 @@
+package JavaRushTaskSt.Zadachi_13.LV.ArrayList;
+
+import java.util.ArrayList;
+
+public class Test {
+    //создать объект класса ArrayList, нужно написать код типа:
+    //ArrayList<Тип> имя = new ArrayList<Тип>();
+
+    ArrayList<Integer> list = new ArrayList<Integer>();//Список целых чисел
+    ArrayList<String> list1 = new ArrayList<String>();//Список строк
+    ArrayList<Double> list2 = new ArrayList<Double>();//Список вещественных чисел
+}
